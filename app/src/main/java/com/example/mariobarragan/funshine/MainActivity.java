437071e3c88f7b704c0f4a645ff58d14aa468dd6
Mario Barragán <albeto8@gmail.com>
2017-04-10 12:33:05 -0500

    Implementing Volley for web requests

--- app/src/main/java/com/example/mariobarragan/funshine/MainActivity.java
+++ app/src/main/java/com/example/mariobarragan/funshine/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.mariobarragan.funshine;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
+    final String URL_COORD = "?lat=19.411653&lon=-99.170168";
+    final String URL_UNITS = "&units=metric";
+    final String API_KEY = "&appid=a454a001a64c63c75ca9f11f563fa108";
+    //19.411653, -99.170168
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = URL_BASE + URL_COORD + URL_UNITS + API_KEY;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.v("FUN", "Response: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("FUN", "Error: " + error.getLocalizedMessage());
+            }
+        });
+
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
     }
 }

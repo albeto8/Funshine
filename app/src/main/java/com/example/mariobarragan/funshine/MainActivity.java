0cf77e2d59c7e278b43bf2884767f20c48ab5e15
Mario Barragán <albeto8@gmail.com>
2017-04-11 10:40:58 -0500

    Use GPS to get weather data

--- app/src/main/java/com/example/mariobarragan/funshine/MainActivity.java
+++ app/src/main/java/com/example/mariobarragan/funshine/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.mariobarragan.funshine;
 
+import android.*;
+import android.Manifest;
+import android.app.Activity;
 import android.app.DownloadManager;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -11,24 +21,40 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
+    //final String URL_COORD = "?lat=19.411653&lon=-99.170168";
     final String URL_COORD = "?lat=19.411653&lon=-99.170168";
     final String URL_UNITS = "&units=metric";
     final String API_KEY = "&appid=a454a001a64c63c75ca9f11f563fa108";
     //19.411653, -99.170168
+
+    private GoogleApiClient mGoogleApiClient;
+    private final int PERMISSION_LOCATION = 111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String url = URL_BASE + URL_COORD + URL_UNITS + API_KEY;
+        mGoogleApiClient = new GoogleApiClient.Builder(this).
+                addApi(LocationServices.API).enableAutoManage(this, this).
+                addConnectionCallbacks(this).addOnConnectionFailedListener(this).
+                build();
+    }
 
+    public void downloadWeatherData(Location location) {
+        final String fullCoords = URL_COORD + location.getLatitude() + "&lon=" + location.getLongitude();
+        String url = URL_BASE + URL_COORD + URL_UNITS + API_KEY;
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -40,7 +66,55 @@ public class MainActivity extends AppCompatActivity {
                 Log.v("FUN", "Error: " + error.getLocalizedMessage());
             }
         });
-
         Volley.newRequestQueue(this).add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_LOCATION);
+        } else {
+            startLocationServices();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        downloadWeatherData(location);
+    }
+
+    public void startLocationServices() {
+        try {
+            LocationRequest req = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, req, this);
+        } catch (SecurityException exception) {
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationServices();
+                } else {
+                    // show dialog you denied permissions
+                    Toast.makeText(this, "I can't run your location dummy you denied permission!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
